88ee610786430e0f1beb051cbd736a133f3e92e7
Luka Važić <lukavazic@lukavazic-macbookpro.roam.corp.google.com>
2016-12-17 10:28:36 -0800

    Updated version number

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ setup(
   install_requires=[
           'numpy',
       ],
-  version = '0.91',
+  version = '0.92',
   description = 'Simple Non-Negative Matrix Algorithm Implementation Using NumPy',
   author = 'Luka Vazic',
   author_email = 'lvazic@gmail.com',
   url = 'https://github.com/lvazic/simplenmf',
-  download_url = 'https://github.com/lvazic/lightnmf/tarball/0.91',
+  download_url = 'https://github.com/lvazic/lightnmf/tarball/0.92',
   keywords = ['nmf', 'non-negative matrix factorization'],
   classifiers = [],
 )
